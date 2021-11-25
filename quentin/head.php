100f2558faeaa1cd399951d98dd4d31aa1b2d8b7
KingPrime69 <qripot@gaming.tech>
2021-11-25 14:51:25 +0100

    plein de require

---
+++ quentin/head.php
@@ -0,0 +1,11 @@
+<head>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Code+Latin:wght@500&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Webfolio: Projet Algorithmie</title>
+</head>
---
+++ quentin/index.php
@@ -0,0 +1,135 @@
+<?php require_once "init.php"; ?>
+<html>
+<?php require_once "head.php"; ?>
+  <body>
+    <div class="bg">
+    <?php require_once "header.php"; ?>
+    <div class="row">
+      <div class="col s10 m10 l10 offset-s1 offset-m1 offset-l1 bg_main">
+        <h2 class="s12 m12 l12">Présentation Equipe</h2>
+        <div class="bio">
+          <div class="col s10 m8 l8 offset-l2 carousel carousel-slider center">
+            <a class="col s12 button btn transparent z-depth-0 carousel-slider">
+              <i class="blue-grey waves-effect waves-light material-icons left show" id="button_preteaml">arrow_back</i>
+              <i class="blue-grey waves-effect waves-light material-icons right show" id="button_nextteaml">arrow_forward</i>
+            </a>
+            <div class="carousel-item grey darken-4 white-text" href="#one!">
+              <img class="col s12 m5 l5" src="img/lolo.jpg" alt="Projet">
+              <h3>Martina Lorenzo</h3>
+              <p class="white-text">-Obtention du Bac Technologique STI2D, 2021 <br>
+              -Etudiant au Gaming Campus, Lyon</p>
+            </div>
+            <div class="carousel-item grey darken-4 white-text" href="#two!">
+              <img class="col s12 m5 l5" src="img/pp.jpg" alt="Projet">
+              <h3>Ripot Quentin</h3>
+              <p class="white-text">-Obtention du Bac Technologique STI2D, 2021 <br>
+              -Etudiant au Gaming Campus, Lyon <br>
+              -Dev Web dans l'assossiation 2Rivials</p>
+            </div>
+          </div>
+        </div>
+        <div class="projectpyth">
+          <div class="col s12 m12 l12 parallax-container">
+            <div class="parallax">
+              <img src="img/prog.jpg" alt="Projet">
+            </div>
+          </div>
+          <h2 class="col s12 m12 l12">Algorithimie, Projet 1</h2>
+          <h3 class="col s12 m12 l12">Ripot Quentin, Martina Lorenzo</h3>
+          <div class="algo">
+            <div class="col s12 m12 l6 offset-l1 carousel carousel-slider">
+              <a class="col s12 button btn transparent z-depth-0 carousel-slider">
+                <i class="blue-grey waves-effect waves-light material-icons left show" id="button_pre1">arrow_back</i>
+                <i class="blue-grey waves-effect waves-light material-icons right show" id="button_next1">arrow_forward</i>
+              </a>
+              <a class="col s12 m12 l12 carousel-item" href="#one!"><img src="img/fond6.jpg" alt="Projet"></a>
+              <a class="col s12 m12 l12 carousel-item" href="#two!"><img src="img/fond1.jpg" alt="Projet"></a>
+              <a class="col s12 m12 l12 carousel-item" href="#three!"><img src="img/fond7.jpg" alt="Projet"></a>
+              <a class="col s12 m12 l12 carousel-item" href="#four!"><img src="img/fond3.jpg" alt="Projet"></a>
+              <a class="col s12 m12 l12 carousel-item" href="#five!"><img src="img/fond9.jpg" alt="Projet"></a>
+            </div>
+            <p class="col s12 m12 l4 flow-text">Lors de ce projet nous avons d'abord appris l'Algorithmie.
+            Nous avons donc assimilé différentes notions, 4 plus particulièrement.
+            Nous avons les Variables, Les Conditions, Les Boucles ainsi que les Fonctions.</p>
+          </div>
+        </div>
+        <div class="projectc">
+          <div class="col s12 m12 l12 parallax-container">
+            <div class="parallax">
+              <img src="img/c.jpg" alt="Projet">
+            </div>
+          </div>
+          <h2 class="col push l12">Programmation en C, Projet 2</h2>
+          <h3 class="col s12 m12 l12">Ripot Quentin, Martina Lorenzo</h3>
+          <div class="C">
+          <div class="col s12 m12 l6 offset-l1 carousel carousel-slider">
+            <a class="col s12 button btn transparent z-depth-0 carousel-slider">
+              <i class="blue-grey waves-effect waves-light material-icons left show" id="button_preC">arrow_back</i>
+              <i class="blue-grey waves-effect waves-light material-icons right show" id="button_nextC">arrow_forward</i>
+            </a>
+            <a class="carousel-item" href="#one!"><img src="img/C3.jpg" alt="Projet"></a>
+            <a class="carousel-item" href="#two!"><img src="img/fond16.jpg" alt="Projet"></a>
+            <a class="carousel-item" href="#three!"><img src="img/debian.jpg" alt="Projet"></a>
+            <a class="carousel-item" href="#four!"><img src="img/fond18.jpg" alt="Projet"></a>
+            <a class="carousel-item" href="#five!"><img src="img/C1.jpg" alt="Projet"></a>
+          </div>
+        </div>
+          <p class="col s12 m12 l4 flow-text">Projet de programmation en C, crée un Puissance 4 sous environnement Linus via Git/github</p>
+        </div>
+        <div class="projectweb">
+          <div class="col s12 m12 l12 parallax-container">
+            <div class="parallax">
+              <img src="img/html.png" alt="Projet">
+            </div>
+          </div>
+          <h2 class="col s12 m12 l12">Site Web, Projet 3</h2>
+          <h3 class="col s12 m6 l6">Ripot Quentin</h3>
+          <div class="hide-on-small-only">
+            <h3 class="col s12 m6 l6 ">Martina Lorenzo</h3>
+          </div>
+          <div class="web">
+            <div class="quentin">
+            <div class="col s12 m5 l5 carousel carousel-slider">
+              <a class="col s12 button btn transparent z-depth-0 carousel-slider">
+                <i class="blue-grey waves-effect waves-light material-icons left show" id="button_preq">arrow_back</i>
+                <i class="blue-grey waves-effect waves-light material-icons right show" id="button_nextq">arrow_forward</i>
+              </a>
+              <a class="carousel-item" href="#one!"><img src="img/mainq.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#two!"><img src="img/blueq.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#three!"><img src="img/greenq.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#four!"><img src="img/redq.jpg" alt="Projet"></a>
+            </div>
+          </div>
+          <div class="hide-on-med-and-up">
+            <h3 class="col s12 ">Martina Lorenzo</h3>
+          </div>
+          <div class="lorenzo">
+            <div class="col s12 m5 l5 offset-m1 offset-l1 carousel carousel-slider web">
+              <a class="col s12 button btn transparent z-depth-0 carousel-slider"><i class="blue-grey waves-effect waves-light material-icons left show" id="button_prel">arrow_back</i><i class="blue-grey waves-effect waves-light material-icons right show" id="button_nextl">arrow_forward</i></a>
+              <a class="carousel-item" href="#one!"><img src="img/site1.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#two!"><img src="img/site2.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#three!"><img src="img/site3.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#four!"><img src="img/site4.jpg" alt="Projet"></a>
+              <a class="carousel-item" href="#five!"><img src="img/site5.jpg" alt="Projet"></a>
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="click-to-toggle fixed-action-btn">
+      <a class="btn-floating btn-small transparent">
+      </a>
+      <ul>
+        <li><p class="left">Sale fouineur</p></li>
+      </ul>
+    </div>
+
+      <!-- Tap Target Structure -->
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </body>
+</html>
