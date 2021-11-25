c93bc328a34841269530fbf353dcff4550cdb1ce
XiiNox83 <lmartina@gaming.tech>
2021-11-25 15:54:59 +0100

    b

--- lorenzo/Projet 2.php
+++ lorenzo/Projet 2.php
@@ -1,80 +1,10 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
-      <link rel="stylesheet" href="css/style.css">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Code+Latin:wght@500&display=swap" rel="stylesheet">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Webfolio: Projet C</title>
-    </head>
-    <body>
-      <div class="bg">
-        <ul id="slide-out" class="sidenav">
-          <li><div class="user-view">
-            <div class="background">
-              <img src="img/fond.jpg" alt="Projet">
-            </div>
-            <div class="name">
-              <a href="#user"><img class="circle" src="img/manette2.jpg"></a>
-              <a href="#name"><span class="white-text name">Martina Lorenzo</span></a>
-              <a href="#email"><span class="white-text email">lmartina@gaming.tech</span></a>
-              <a href="#name"><span class="white-text name">Ripot Quentin</span></a>
-              <a href="#email"><span class="white-text email">qripot@gaming.tech</span></a>
-            </div>
-          </div></li>
-          <li><a href="index.html"><i class="material-icons">arrow_forward</i>Présentation</a></li>
-          <li><div class="divider"></div></li>
-          <li><a href="Projet 1.html"><i class="material-icons">bookmark</i>Projet 1</a></li>
-          <li><a href="Projet 2.html"><i class="material-icons">bookmark</i>Projet 2</a></li>
-          <li><a href="Projet 3.html"><i class="material-icons">bookmark</i>Projet 3</a></li>
-        </ul>
-        <header>
-          <div class="row">
-              <div class="col s12 m12 l12 trans hide-on-small-only"></div>
-              <div class="col s12 m12 l12 title ">
-              <a href="#" data-target="slide-out" class="col l1 sidenav-trigger"><i class="material-icons">menu</i></a>
-                <h1 class="col m8 l8 offset-l1 hide-on-small-only">Webfolio: Projets Gaming Campus</h1>
-                <p class="hide-on-med-and-up col s8">Webfolio: Projets Gaming Campus</p>
-                <a class="right cyan accent-1 waves-effect waves-light btn modal-trigger" href="#modal1">Nous Contacter</a>
-                <div id="modal1" class="modal bottom-sheet">
-                  <div class="col s12 m12 l12 modal-content">
-                    <form class="col s6 offset-s3">
-                      <div class="input-field col s5">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input id="icon_prefix" type="text" class="validate">
-                        <label for="icon_prefix">Nom</label>
-                      </div>
-                      <div class="input-field col s5 offset-s1">
-                        <i class="material-icons prefix">account_box</i>
-                        <input id="last_name" type="text" class="validate">
-                        <label for="last_name">Prénom</label>
-                      </div>
-                      <div class="input-field col s11">
-                        <i class="material-icons prefix">contact_mail</i>
-                         <input id="email" type="email" class="validate">
-                         <label for="email">Email</label>
-                         <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                       </div>
-                       <div class="input-field col s8">
-                        <i class="material-icons prefix">mode_edit</i>
-                        <textarea id="textarea2" class="materialize-textarea" data-length="120"></textarea>
-                        <label for="textarea2">Message</label>
-                      </div>
-                      <button id="send" class="col l2 offset-l1 btn waves-effect waves-light send" type="submit" name="action">
-                        <i class="material-icons left">send</i>
-                        envoyer
-                      </button>
-                    </div>
-                    </form>
-                  </div>
-              </div>
-              <div class="col s12 m12 l12 trans hide-on-small-only"></div>
-          </div>
-        </header>
+<?php require_once "init.php"; ?>
+<html>
+<?php require_once "head.php"; ?>
+  <body>
+    <div class="bg">
+    <?php require_once "header.php"; ?>
         <main>
           <div class="row">
             <div class="col s10 m10 l10 offset-s1 offset-m1 offset-l1 bg_main">
