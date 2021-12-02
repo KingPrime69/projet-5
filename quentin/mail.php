a5ff497d7792da35e25a637d78e48ca7bf2b1e7a
KingPrime69 <qripot@gaming.tech>
2021-12-02 13:13:00 +0100

    end end end end end end

--- quentin/mail.php
+++ quentin/mail.php
@@ -2,12 +2,10 @@
 require_once "init.php";
 $surname = $_SESSION['user']['surname'];
 $email = $_SESSION['user']['email'];
-$subject = $_POST['object'];
-$message = $_POST['textarea2'];
+$objets = $_POST['object'];
+$content = $_POST['textarea2'];
 $headers = array('MIME-Version: 1.0','Content-type: text/html; charset=utf8');
-$to = "qripot@gaming.tech";
-echo $to;
-if(mail($email,$subject,$message,$headers)){
+if(mail($surname,$email,$objet,$content,$headers)){
 }else{
     //on a une erreur quelque part
 }
