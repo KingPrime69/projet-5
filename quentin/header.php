bee02232647bbb1c6694db27c25bf4d09b481721
KingPrime69 <qripot@gaming.tech>
2021-12-02 13:10:47 +0100

    end

--- quentin/header.php
+++ quentin/header.php
@@ -26,15 +26,52 @@
       }
     }
     ?>
+    <?php
+    if(!empty($_SESSION['user'])){
+      ?>
+      <li><a class="cyan accent-1 waves-effect waves-light btn modal-trigger black-text" href="#modal1">Nous Contacter</a></li>
+      <?php
+    }
+    ?>
   </ul>
+  <div class="row">
+    <div id="modal1" class="modal bottom-sheet">
+      <div class="col s12 m12 l12 modal-content">
+        <form class="col s6 offset-s4" method="post" action="mail.php">
+          <div class="input-field col s8 ">
+            <i class="material-icons prefix cyan-text">account_box</i>
+            <label class="cyan-text" for="Surname"><?php echo $_SESSION['user']['surname']; ?></label>
+          </div>
+          <div class="input-field col s8 ">
+            <i class="material-icons prefix cyan-text">contact_mail</i>
+            <label class="cyan-text" for="Surname"><?php echo $_SESSION['user']['email']; ?></label>
+          </div>
+          <div class="input-field col s8">
+            <i class="material-icons prefix cyan-text">email</i>
+            <input id="icon_prefix" class="cyan-text" type="text" name="object">
+            <label for="icon_prefix" class="cyan-text">Objet</label>
+          </div>
+          <div class="input-field col s8">
+            <i class="material-icons prefix cyan-text">mode_edit</i>
+            <textarea id="textarea2" class="materialize-textarea cyan-text" data-length="120" name="textarea2"></textarea>
+            <label for="textarea2" class="cyan-text">Message</label>
+          </div>
+          <button id="send" class="col l3 btn cyan accent-1 waves-effect waves-light btn black-text" type="submit" name="Me connecter">
+            <i class="material-icons left">send</i>
+            envoyer
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   <header>
     <div class="row">
-        <div class="col s12 m12 l12 trans hide-on-small-only"></div>
-        <div class="col s12 m12 l12 title ">
+      <div class="col s12 m12 l12 trans hide-on-small-only"></div>
+      <div class="col s12 m12 l12 title ">
         <a href="#" data-target="slide-out" class="col l1 sidenav-trigger"><i class="material-icons">menu</i></a>
           <h1 class="col m8 l8 offset-l1 hide-on-small-only">Webfolio: Projets Gaming Campus</h1>
           <p class="hide-on-med-and-up col s8">Webfolio: Projets Gaming Campus</p>
         <div class="col s12 m12 l12 trans hide-on-small-only"></div>
+      </div>
     </div>
-  </div>
   </header>
---
+++ quentin/mail.php
@@ -0,0 +1,16 @@
+<?php
+require_once "init.php";
+$surname = $_SESSION['user']['surname'];
+$email = $_SESSION['user']['email'];
+$subject = $_POST['object'];
+$message = $_POST['textarea2'];
+$headers = array('MIME-Version: 1.0','Content-type: text/html; charset=utf8');
+$to = "qripot@gaming.tech";
+echo $to;
+if(mail($email,$subject,$message,$headers)){
+);
+}else{
+    //on a une erreur quelque part
+}
+header('Location:index.php');
+?>
